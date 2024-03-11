125c69c8d50ada24df88685a9c0c95001042c03d
Harsh Modi <harshmodi0297@gmail.com>
2024-03-10 18:07:14 -0700

    Basic Text Extraction Feature Ready

--- textextraction/models.py
+++ textextraction/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class UploadedFile(models.Model):
+    file = models.FileField(upload_to='uploads/')
+
+class ExtractedText(models.Model):
+    file = models.ForeignKey(UploadedFile, on_delete=models.CASCADE)
+    parameter = models.CharField(max_length=100)
+    value = models.TextField()
